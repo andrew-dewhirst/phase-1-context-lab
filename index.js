266b19d5e1a7736831a1bd1a8803bb40dd971a8f
Andrew Dewhirst <andrew.dewhirst8@gmail.com>
2021-09-30 22:26:16 -0400

    commit tree b7060d08f32ea8422ba140adf55370e4875afecc

--- index.js
+++ index.js
@@ -1,4 +1,52 @@
-/* Your Code Here */
+function createEmployeeRecord([firstName,lastName,title,payRate]){
+    let employee = {
+      firstName: firstName,
+      familyName: lastName,
+      title: title,
+      payPerHour: payRate,
+      timeInEvents:[],
+      timeOutEvents:[]
+    }
+    return employee
+  };
+
+  function createEmployeeRecords(arrayOfNames){
+    return arrayOfNames.map(array => {
+    let records = createEmployeeRecord(array)
+    return records
+    })
+  };
+
+  function createTimeInEvent(timestamp){
+    this.timeInEvents.push ({
+      type: "TimeIn",
+      date: timestamp.split(" ")[0],
+      hour: parseInt(timestamp.split(" ")[1], 10)
+      })
+      return this
+  };
+
+  function createTimeOutEvent(timestamp){
+    this.timeOutEvents.push ({
+      type: "TimeOut",
+      date: timestamp.split(" ")[0],
+      hour: parseInt(timestamp.split(" ")[1], 10)
+    })
+    return this
+  };
+
+  function hoursWorkedOnDate(date){
+    let startingHourByDate = this.timeInEvents.filter(user => user.date.startsWith(date));
+    let endingHourByDate = this.timeOutEvents.filter(user => user.date.startsWith(date));
+    let totalHoursWorked = (endingHourByDate[0].hour - startingHourByDate[0].hour)/100
+  
+    return totalHoursWorked;
+  };
+
+  function wagesEarnedOnDate(date){
+    let hoursWorked = hoursWorkedOnDate.call(this,date);
+    return hoursWorked * this.payPerHour
+  };
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -21,3 +69,14 @@ const allWagesFor = function () {
     return payable
 }
 
+function findEmployeeByFirstName(srcArray,firstName){
+    const employeeFinder = srcArray.filter(employee => firstName == employee.firstName)
+    return employeeFinder[0];
+};
+
+function calculatePayroll(allEmployees){
+    let totalWages = allEmployees.map(employee => allWagesFor.call(employee));
+    let reducer = (firstValue,nextValue) => firstValue + nextValue;
+      let companyPayroll = totalWages.reduce(reducer,0);
+     return companyPayroll;
+    };
